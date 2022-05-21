06b56f64be1b9a075934331ad8705c345c7a3616
casper <ahmadsy88@gmail.com>
2022-05-22 00:10:49 +0300

    feat: add filtering functionality

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,35 +6,34 @@ import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
-    const[yearFiltered,setyearFiltered] = useState('2020');
-    const filterByYear = (newYearFiltered)=>{
+    const [yearFiltered, setyearFiltered] = useState('2020');
+    const filterByYear = (newYearFiltered) => {
         setyearFiltered(newYearFiltered);
     }
-  return (
-    <Card className="expenses">
-        <ExpensesFilter selected={yearFiltered}  onChangeFilter = {filterByYear}/>
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
-    </Card>
-  );
+
+    const YearFilterdList = props.items.filter((expense) => {
+        return expense.date.getFullYear().toString() === yearFiltered;
+    });
+
+    let renderedExpenses = <p>No expenses to show.</p>;
+
+    if (YearFilterdList.length > 0) {
+        renderedExpenses = YearFilterdList.map(
+            (expense) =>
+                <ExpenseItem
+                    title={expense.title}
+                    amount={expense.amount}
+                    date={expense.date}
+                />)
+
+    }
+
+    return (
+        <Card className="expenses">
+            <ExpensesFilter selected={yearFiltered} onChangeFilter={filterByYear}/>
+            {renderedExpenses}
+        </Card>
+    );
 }
 
 export default Expenses;
