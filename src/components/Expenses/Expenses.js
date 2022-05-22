9eb8a4356567e2b35f872488c4a97229b4c9c73e
casper <ahmadsy88@gmail.com>
2022-05-22 08:47:00 +0300

    refactor: add component to handle filtered list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
+import ExpenseList from "./ExpenseList";
 
 const Expenses = (props) => {
     const [yearFiltered, setyearFiltered] = useState('2020');
@@ -15,23 +16,11 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === yearFiltered;
     });
 
-    let renderedExpenses = <p>No expenses to show.</p>;
-
-    if (YearFilterdList.length > 0) {
-        renderedExpenses = YearFilterdList.map(
-            (expense) =>
-                <ExpenseItem
-                    title={expense.title}
-                    amount={expense.amount}
-                    date={expense.date}
-                />)
-
-    }
 
     return (
         <Card className="expenses">
             <ExpensesFilter selected={yearFiltered} onChangeFilter={filterByYear}/>
-            {renderedExpenses}
+            <ExpenseList  items={YearFilterdList}/>
         </Card>
     );
 }
